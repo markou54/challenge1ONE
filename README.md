1fc959d60f19b35e155e50d9649ae0eba8cb8799
Marco <119984721+markou54@users.noreply.github.com>
2023-01-22 18:07:37 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1><em>Challenge 1 ONE - Encriptador y desencriptador de texto</em></h1>
+
+El proyecto se trata de un encriptador de texto con diseño responsive.
+
+<h3>Proyecto realizado con:</h3>
+
+<ul>
+  <li>HTML</li>
+  <li>JavaScript</li>
+  <li>CSS</li>
+</ul>  
+
+<h3>Contacto y GitHub Pages del proyecto</h3>
+
+
+
+<ul>
+  <li>Si deseas ver el proyecto antes de descargarlo miralo aquí: <a href="https://santiagohendz.github.io/Encriptador-HTML-y-CSS/">GitHub Pages</a></li>
+  <li>Si tienes alguna duda puedes contactarme aquí: <a href="https://www.linkedin.com/in/marco-antonio-urbán-rico-434512136/">LinkedIn</a></li>
+</ul>
---
+++ docs/script.js
@@ -0,0 +1,77 @@
+const areaTexto = document.querySelector(".areatexto");
+const areaEncriptado = document.querySelector(".areaencriptado");
+
+const botonEncriptar = document.querySelector(".botonencriptar");
+const botonDesencriptar = document.querySelector(".botondesencriptar");
+const botonCopiar = document.querySelector(".botoncopiar");
+
+const imgBuscando = document.getElementById("imgbuscando");
+const msgNoHayTexto = document.getElementById("msgnohaytexto");
+
+function analizarMensaje(){
+  var mensaje1 = areaTexto.value;
+  var mensaje2 = areaEncriptado.value;
+  var caracteres = "ºª!¡@#·~$%&/()=¿?^[]`+{}¨ç;,:._-1234567890abcdefghijklmnñopqrstuvwxyz \\\'\v\t\b\´\n\"";
+  var mensajeErroneo1= "";
+  var mensajeErroneo2= "";
+  var validador = true;
+
+  for(var caracter of mensaje1){
+    if(!caracteres.includes(caracter)){
+      mensajeErroneo1 = "ERROR"
+    }
+  }
+
+  for(var caracter of mensaje2){
+    if(!caracteres.includes(caracter)){
+      mensajeErroneo2 = "ERROR"
+    }
+  }
+
+  if (mensajeErroneo1 == "ERROR" || mensajeErroneo2 == "ERROR"){
+    validador = false;
+    alert("Ingrese carácteres válidos por favor");
+  }
+
+  return validador;
+}
+
+function encriptar(){
+  if(analizarMensaje() == false) return;
+
+  var mensaje = areaTexto.value;
+  var mensajeEncriptado = mensaje.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");
+
+  areaEncriptado.style.display = "block";
+  botonCopiar.style.display = "block";
+
+  imgBuscando.hidden = true;  
+  msgNoHayTexto.hidden = true;  
+
+  areaEncriptado.value = mensajeEncriptado;
+}
+
+function desencriptar(){
+  if(analizarMensaje() == false) return;
+
+  var mensajeEncriptado = areaTexto.value;
+  var mensajeDesencriptado = mensajeEncriptado.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");
+
+  areaEncriptado.style.display = "block";
+  botonCopiar.style.display = "block";
+
+  imgBuscando.hidden = true;  
+  msgNoHayTexto.hidden = true;  
+  
+  areaEncriptado.value = mensajeDesencriptado;
+}
+
+function copiar(){
+  var mensajeEncriptado = areaEncriptado.value;
+  
+  navigator.clipboard.writeText(mensajeEncriptado);
+}
+
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+botonCopiar.onclick = copiar;
